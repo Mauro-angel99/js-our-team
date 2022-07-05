e7a44a8bf65f81c216d9ab5bf8aecb780429909b
mauro-angel99 <angelottimauro@gmail.com>
2022-07-05 15:21:20 +0200

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const team = [
+    {
+        name: `Wayne Barnett`,
+        job: `Founder & CEO`,
+        image: `wayne - barnett - founder - ceo.jpg`,
+    },
+    {
+        name: `Angela Caroll`,
+        job: `Chief Editor`,
+        image: `angela-caroll-chief-editor.jpg`,
+    },
+    {
+        name: `Walter Gordon`,
+        job: `Office Manager`,
+        image: `walter-gordon-office-manager.jpg`,
+    },
+    {
+        name: `Angela Lopez`,
+        job: `Social Media Manager`,
+        image: `angela-lopez-social-media-manager.jpg`,
+    },
+    {
+        name: `Scott Estrada`,
+        job: `Developer`,
+        image: `scott-estrada-developer.jpg`,
+    },
+    {
+        name: `Barbara Ramos`,
+        job: `Graphic Designer`,
+        image: `barbara-ramos-graphic-designer.jpg`,
+    },
+]
+
+
+
+console.log(team)
